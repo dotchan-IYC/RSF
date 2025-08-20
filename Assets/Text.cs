77f4b28de9b8ace5bf9b8202f66d1ff7092ff20c
songyunhyun <sans10qp10qp@gmail.com>
2025-08-20 19:19:59 +0900

    feat002

---
+++ Assets/Text.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    TextMeshProUGUI _renderer;
+    int r = 255;
+    int g;
+    int b;
+    bool aaa;
+    
+
+    private void Awake()
+    {
+        _renderer = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(ColorChange());
+    }
+    private void Update()
+    {
+        _renderer.color = new Color(r / 255f, g / 255f, b / 255f, 1);        
+        //g ¥√∞Ì r ¡Ÿ∞Ì b ¥√∞Ì g ¡Ÿ∞Ì r ¥√∞Ì b ¡Ÿ∞Ì
+    }
+
+    IEnumerator ColorChange()
+    {
+        while (g !=255)
+        {
+            g++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        while (r != 0)
+        {
+            r--;
+            yield return new WaitForSeconds(0.01f);
+        }
+        while (b != 255)
+        {
+            b++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        while (g != 0)
+        {
+            g--;
+            yield return new WaitForSeconds(0.01f);
+        }
+            while (r !=255)
+        {
+            r++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        while (b != 0)
+        {
+            b--;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        StartCoroutine(ColorChange());
+        yield break;
+        
+    }
+}
